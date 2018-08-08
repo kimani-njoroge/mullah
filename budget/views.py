fba79f90f6637ce65b40f73ecf895e15be0fae0e
kimani-njoroge <kimani064@gmail.com>
2018-08-08 14:23:17 +0300

    post transaction form

--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,29 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from budget.models import Transaction
+from .forms import CategoryForm, TransactionForm
+
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = CategoryForm(request.POST,request.FILES)
+        tform = TransactionForm(request.POST,request.FILES)
+        if all([form.is_valid(),tform.is_valid]):
+            category = form.save(commit=False)
+            transaction = form.save(commit=False)
+            category.user = current_user
+            transaction.user = current_user
+            category.save()
+            transaction.save()
+    else:
+        form = CategoryForm()
+        tform = TransactionForm()
+    return render(request,'post.html',{"form":form,"tform":tform})
