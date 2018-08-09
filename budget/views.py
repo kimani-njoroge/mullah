7ba1e774a49fafd0666cc9c0ecf8eede07869ba8
kimani-njoroge <kimani064@gmail.com>
2018-08-09 15:40:14 +0300

    fetch transactions based on the user

--- budget/views.py
+++ budget/views.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from budget.models import Transaction
 from .forms import  TransactionForm
+import csv from django.http import HttpResponse
 
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
-def index(request,user_id):
-    transactions = Transaction.objects.all()
+def index(request):
+    user = request.user
+    transactions = Transaction.objects.filter(user=user)
     print(transactions)
-    total = Transaction.objects.aggregate(Sum('price'))['price__sum']
+    tots = Transaction.objects.filter(user=user)
+    total = tots.aggregate(Sum('price'))['price__sum']
+    print(total)
     return render(request,'index.html',{"transactions":transactions,"total":total})
 
 @login_required(login_url='/accounts/login/')
@@ -23,6 +27,7 @@ def post(request):
             transaction = form.save(commit=False)
             transaction.user = current_user
             transaction.save()
+            return redirect(index)
     else:
         form = TransactionForm()
     return render(request,'post.html',{"form":form})
